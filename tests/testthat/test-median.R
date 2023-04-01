c953365b9dde71f6a1a1b833da4795ab993ac59b
Lukas Jung <jung-lukas@gmx.net>
2023-04-01 23:05:38 +0200

    rewrite all tests without the pipe
    * This will enable naidem to run on R versions less than 4.1.

--- tests/testthat/test-median.R
+++ tests/testthat/test-median.R
@@ -22,49 +22,49 @@ x17 <- c(1, 1, 2, NA, NA, NA)
 # Actual medians (i.e., the main function) --------------------------------
 
 test_that("the default method returns non-`NA` values when it should", {
-  x1 |> median_na() |> expect_equal(1)
-  x2 |> median_na() |> expect_equal(1)
-  x4 |> median_na() |> expect_equal(0)
-  x5 |> median_na() |> expect_equal(1)
-  x6 |> median_na() |> expect_equal(1)
-  x8 |> median_na() |> expect_equal(5.8)
+  expect_equal(median_na(x1), 1)
+  expect_equal(median_na(x2), 1)
+  expect_equal(median_na(x4), 0)
+  expect_equal(median_na(x5), 1)
+  expect_equal(median_na(x6), 1)
+  expect_equal(median_na(x8), 5.8)
 })
 
 test_that("the default method returns `NA` when it should", {
-  x3  |> median_na() |> expect_equal(NA_real_)
-  x7  |> median_na() |> expect_equal(NA_real_)
-  x9  |> median_na() |> expect_equal(NA_real_)
-  x10 |> median_na() |> expect_equal(NA_real_)
-  x11 |> median_na() |> expect_equal(NA_real_)
+  expect_equal(median_na(x3 ), NA_real_)
+  expect_equal(median_na(x7 ), NA_real_)
+  expect_equal(median_na(x9 ), NA_real_)
+  expect_equal(median_na(x10), NA_real_)
+  expect_equal(median_na(x11), NA_real_)
 })
 
 
 # Possible medians --------------------------------------------------------
 
 test_that("`median_possible_values()` works correctly with even lengths", {
-  x12 |> median_possible_values() |> expect_equal(c(7, 7.5, 8.5, 9))
-  x13 |> median_possible_values() |> expect_equal(c(7, 7.5))
+  expect_equal(median_possible_values(x12), c(7, 7.5, 8.5, 9))
+  expect_equal(median_possible_values(x13), c(7, 7.5))
 })
 
 test_that("`median_possible_values()` works correctly with odd lengths", {
-  x14 |> median_possible_values() |> expect_equal(c(7, 8))
-  x15 |> median_possible_values() |> expect_equal(c(7, 9))
+  expect_equal(median_possible_values(x14), c(7, 8))
+  expect_equal(median_possible_values(x15), c(7, 9))
 })
 
 test_that("`median_range()` works correctly with even lengths", {
-  x12 |> median_range() |> expect_equal(c(7, 9))
-  x13 |> median_range() |> expect_equal(c(7, 7.5))
+  expect_equal(median_range(x12), c(7, 9))
+  expect_equal(median_range(x13), c(7, 7.5))
 })
 
 test_that("`median_range()` works correctly with odd lengths", {
-  x14 |> median_range() |> expect_equal(c(7, 8))
-  x15 |> median_range() |> expect_equal(c(7, 9))
+  expect_equal(median_range(x14), c(7, 8))
+  expect_equal(median_range(x15), c(7, 9))
 })
 
 test_that("early returns are correct in both possible-medians functions", {
-  x16 |> median_possible_values() |> expect_equal(3)
-  x17 |> median_possible_values() |> expect_equal(NA_real_)
-  x16 |> median_range() |> expect_equal(c(3, 3))
-  x17 |> median_range() |> expect_equal(c(NA_real_, NA_real_))
+  expect_equal(median_possible_values(x16), 3)
+  expect_equal(median_possible_values(x17), NA_real_)
+  expect_equal(median_range(x16), c(3, 3))
+  expect_equal(median_range(x17), c(NA_real_, NA_real_))
 })
 
