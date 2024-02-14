34176fdbdb00ce12def992f3375f7c8559388ecf
Lukas Jung <jung-lukas@gmx.net>
2024-02-14 18:41:08 +0100

    update utils.R helper functions
    * Adapted `NA`-coalescing operators from this blogpost draft: https://github.com/lhdjung/lukas_jung_blog/commit/2e8a4a957cd987ccf562b11d5e7b2ae71803c9a9 (but note that the implementation of `%|%` last mentioned there is named `%|1%` here).
    * Copied the current version of `decrease_na_amount()` from moder: https://github.com/lhdjung/moder/commit/d1836ee7a39d2ea44bece7c63c330782585ca755

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,24 @@
 
+# This NA-coalescing operator replaces any and all `NA` elements of `x` by a
+# safe fallback, `y`. It is named after `rlang::`%|%`()`, although that operator
+# is stricter about types than the one below.
+`%|%` <- function(x, y) {
+  `[<-`(x, is.na(x), y)
+}
+
+# Scalar version of the NA-coalescing operator. It throws an error if `x` is not
+# length 1, which is useful for testing the expectation that it always has
+# length 1.
+`%|1%` <- function(x, y) {
+  if (is.na(x)) {
+    y
+  } else {
+    x
+  }
+}
+
+
+
 # Adapted with modifications from the examples of the `?integer` documentation.
 # For each element of `x`, this vectorized helper checks whether it is
 # *conceptually* an integer (but not necessarily by object type). If `x` is not
@@ -13,16 +33,20 @@ is_whole_number <- function(x, tolerance = .Machine$double.eps^0.5) {
 }
 
 
-# This helper handles the `na.rm.amount` argument in `median2()`. It removes a
-# number of missing values from `x` equal to `na.rm.amount`, then returns `x`.
-decrease_na_amount <- function(x, na.rm, na.rm.amount, na.rm.from) {
+# This helper handles the `na.rm.amount` argument in the proper mode functions:
+# `mode_first()`, `mode_all()`, and `mode_single()`. It removes a number of
+# missing values from `x` equal to `na.rm.amount`, then returns `x`. Notes:
+# -- The specification of `na.rm.from` should be checked by the calling
+# function, like `na.rm.from <- match.arg(na.rm.from)`.
+# -- For efficiency, `decrease_na_amount()` should only be called under very
+# specific conditions, as in `mode_first()` etc.
+decrease_na_amount <- function(x, na.rm, na.rm.amount, na.rm.from = "first") {
   # Check for misspecifications of the calling function's arguments:
   if (na.rm) {
-    msg_error <- paste(
+    stop(paste(
       "Conflicting instructions: `na.rm` removes all missing values,",
       "`na.rm.amount` only removes some number of them."
-    )
-    stop(msg_error)
+    ))
   }
   amount_is_wrong <- length(na.rm.amount) != 1L ||
     !is_whole_number(na.rm.amount) ||
@@ -30,27 +54,32 @@ decrease_na_amount <- function(x, na.rm, na.rm.amount, na.rm.from) {
   if (amount_is_wrong) {
     stop("`na.rm.amount` must be a single whole, non-negative number.")
   }
-  # In vectors without any missing values, there is nothing to remove, so they
-  # should be returned as they are:
-  if (!anyNA(x)) {
+  # Determine the indices of missing values in `x`:
+  na_indices <- which(is.na(x))
+  # Special rules apply in edge cases:
+  # -- Vectors without any `NA`s have nothing to remove, so they should be
+  # returned as they are.
+  # -- In case no `NA`s remain after subtracting `na.rm.amount` from the number
+  # of missing values in `x`, all of them need to be removed from `x`. Taking
+  # `na.rm.from` into account, as below, is not necessary here because this
+  # argument is only relevant if some `NA`s will be left over.
+  if (length(na_indices) == 0L) {
     return(x)
-  }
-  # Record the indices of missing values in `x`:
-  x_missing <- which(is.na(x))
-  # Subtract `na.rm.amount` from the number of missing values:
-  nna <- length(x_missing) - na.rm.amount
-  # In case no `NA`s remain after this subtraction, all of them need to be
-  # removed from `x`:
-  if (nna <= 0L) {
-    return(x[-x_missing])
+  } else if (length(na_indices) <= na.rm.amount) {
+    return(x[-na_indices])
   }
   # Target the indices of those missings that should be ignored:
-  x_missing_but_ignored <- switch(
+  na_indices_ignored <- switch(
     na.rm.from,
-    "first"  = utils::head(x_missing, length(x_missing) - nna),
-    "last"   = utils::tail(x_missing, length(x_missing) - nna),
-    "random" = sample(x_missing, size = length(x_missing) - nna)
+    "first"  = utils::head(na_indices, na.rm.amount),
+    "last"   = utils::tail(na_indices, na.rm.amount),
+    "random" = sample(na_indices, na.rm.amount)
   )
-  # Return `x`, excluding those values:
-  x[-x_missing_but_ignored]
+  # If no `NA`s are to be ignored, `x` should be returned as it is:
+  if (length(na_indices_ignored) == 0L) {
+    return(x)
+  }
+  # Return `x`, excluding the values in question:
+  x[-na_indices_ignored]
 }
+
