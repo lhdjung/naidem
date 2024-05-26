85aed8c4770f101ba75558fba0e7cf7cef27a3b3
Lukas Jung <jung-lukas@gmx.net>
2024-05-26 20:14:15 +0200

    clarify `median_range()`

--- R/median-range.R
+++ R/median-range.R
@@ -15,8 +15,9 @@
 #'   If the median can be determined (i.e., [`median2()`] would return a
 #'   non-`NA` value), `median_range()` returns two identical values.
 #'
-#'   If there are missing values at the median position, the median range cannot
-#'   be determined. The function will then return `c(NA, NA)`.
+#'   If there could be missing values at the median position, the median range
+#'   cannot be determined. The function will then return `c(NA, NA)` but of the
+#'   same type as `x`.
 #'
 #' @name median-range
 #'
@@ -25,8 +26,13 @@
 #' @author Lukas Jung, R Core Team
 #'
 #' @examples
+#' # Lower and upper bounds are known,
+#' # even though `median2()` would return `NA`:
 #' median_range(c(7, 7, 8, 9, NA))
 #' median_range(c(7, 7, 7, 8, 9, 9, NA, NA))
+#'
+#' # Too many missing values, so the range is unknown:
+#' median_range(c(7, 7, 8, 9, NA, NA, NA, NA))
 
 
 median_range <- function(x, na.rm.amount = 0, even = c("mean", "low", "high")) {
@@ -49,26 +55,33 @@ median_range.default <- function(x, na.rm.amount = 0,
   }
   x <- sort(x[!is.na(x)])
   nna <- n - length(x)
-  # Some special rules apply if the number of missing values is either zero or
-  # so high that the `NA`s extend into the median position (if all `NA`s are
-  # either at the start or the end):
-  # -- If all values are known, there is only one possible median, and it can be
-  # determined by `median2()`.
-  # -- If any central value is missing, there is no way to determine the bounds.
+  # Special rules apply to extremely low or high numbers of missing values:
+  # -- If no values are missing, there is only one possible median, and it can
+  # be determined by `median2()`.
+  # -- If the number of missing values is so high that the `NA`s would extend
+  # into the median position if all of them were either at the start or the end,
+  # there is no way to determine the bounds.
   if (nna == 0L) {
     return(rep(median2(x, even = even), length = 2L))
   } else if (any(nna >= half)) {
     return(rep(x[NA_integer_], length = 2L))
   }
   # Compute the bounds by checking what the median would be if all `NA`s were
-  # positioned at the start or the end of `x`:
+  # positioned at the start or the end of `x`. This implementation creates such
+  # hypothetical extreme values that stand in for the `NA`s by subtracting `1`
+  # from the lowest known value and adding it to the greatest known value,
+  # although any other positive number would do.
   bound_lower <- median2(
-    c(rep(x[[1L]] - 1, times = nna), x),
-    na.rm = FALSE, na.rm.amount = na.rm.amount, even = even
+    x = c(rep(x[[1L]] - 1, times = nna), x),
+    na.rm = FALSE,
+    na.rm.amount = na.rm.amount,
+    even = even
   )
   bound_upper <- median2(
-    c(x, rep(x[[length(x)]] + 1, times = nna)),
-    na.rm = FALSE, na.rm.amount = na.rm.amount, even = even
+    x = c(x, rep(x[[length(x)]] + 1, times = nna)),
+    na.rm = FALSE,
+    na.rm.amount = na.rm.amount,
+    even = even
   )
   # Return the bounds:
   c(bound_lower, bound_upper)
