d314038f0e742373df8ffc7538b6fce23536ba9f
Lukas Jung <jung-lukas@gmx.net>
2025-03-22 14:21:44 +0100

    clean up `median_count_na_ignore()`

--- R/median-count-na-ignore.R
+++ R/median-count-na-ignore.R
@@ -73,7 +73,6 @@ median_count_na_ignore <- function(x,
     n_known <- length(x)
     nna <- n - n_known
   } else {
-    # x <- sort(x)
     nna <- as.integer(nna)
     n_known <- n
     n <- n + nna
@@ -89,43 +88,20 @@ median_count_na_ignore <- function(x,
     return(nna)
   }
 
-  # # New attempt -- is it enough to count points of overlap? No, this is too
-  # # simple: it won't detect the difference between, e.g., `c(8, 8, NA, NA, NA)`
-  # # and `c(8, 9, NA, NA, NA)`.
-  # na_all <- rep(x[NA_integer_], times = nna)
-  # nna_tolerated <- length(which(c(na_all, x) == c(x, na_all)))
-  # return(max(0L, nna - nna_tolerated))
-
   # With an odd number of known values, there is only one central index for
   # them. Even so, however, this single value is assigned to two variables
   # because these are needed in case of an even number. This avoids having to
   # split the remainder of the code into one odd-length and one even-length arm.
   n_known_is_even <- n_known %% 2L == 0L
   if (n_known_is_even) {
-    # stop("Even length of known values not yet supported!")
+    # Just using integer division here for integer typing; `n_known / 2L` would
+    # be a double value but otherwise equal.
     half_lower <- n_known %/% 2L
     half_upper <- half_lower + 1L
-    # one_if_even <- 1L
   } else {
     half_lower <- (n_known + 1L) %/% 2L
     half_upper <- half_lower
-    # one_if_even <- 0L
   }
-
-  # # Starting at the central index (or indices), go out "left" and "right", i.e.,
-  # # toward the start and the end of the distribution. Record the numbers of
-  # # steps between the value(s) at the central index / indices and those on the
-  # # left and right. If no match is found, the number of steps that can be taken
-  # # in the respective direction is zero, so `0L` is returned as a safe fallback:
-  # steps_left  <- match(x[half_lower], rev(x[seq_len(half_lower - 1L)]), 0L)
-  # steps_right <- match(x[half_upper], x[(half_upper + add_upper + 1L):n_known], 0L)
-
-  # indices_x_arm <- seq_len(n_known %/% 2L + one_if_even)
-  # left_test_arm  <- x[half_lower] == rev(x[indices_x_arm])
-  # right_test_arm <- x[half_upper] == x[indices_x_arm + half_lower]
-
-  # steps_left  <- match(FALSE, left_test_arm)  - 1L
-  # steps_right <- match(FALSE, right_test_arm) - 1L
 
   # In case of two unequal central values, a single `NA` can shift the median,
   # so all `NA`s must be ignored. This can only occur with an even number of
@@ -155,10 +131,8 @@ median_count_na_ignore <- function(x,
   steps_left  <- count_central_steps(x[half_lower] == x[rev(seq_len(half_upper - 1L))])
   steps_right <- count_central_steps(x[half_upper] == x[(half_lower + 1L):n_known])
 
-  # # Calculate the maximal number of `NA`s that can be tolerated -- i.e., that
-  # # don't need to be ignored -- when attempting to determine the median:
-  # nna_tolerated <- 2L * min(steps_left, steps_right)
-
+  # Calculate the maximal number of `NA`s that can be tolerated -- i.e., that
+  # don't need to be ignored -- when attempting to determine the median:
   nna_tolerated <- if (n_known_is_even) {
     min(steps_left, steps_right)
   } else {
@@ -179,12 +153,8 @@ median_count_na_ignore <- function(x,
     "total"      = nna_ignored,
     "proportion" = nna_ignored / nna
   )
+
 }
-
-
-# Helper function used within `median_count_na_ignore()`. If the output of
-# `match()` is `NA`, provide an appropriate replacement based on the logic of
-# the two "arms" of the distribution:
 
 
 #' Count steps from the central value where the same value is still found
@@ -219,12 +189,9 @@ count_central_steps <- function(test_arm) {
   # }
 
   if (!is.na(n_steps)) {
-    return(n_steps)
-  }
-
-  # message("n_steps derived, not directly counted")
-
-  if (length(test_arm) == 0L || all(test_arm)) {
+    n_steps
+    # message("n_steps derived, not directly counted")
+  } else if (length(test_arm) == 0L || all(test_arm)) {
     # message("Length of test arm returned!")
     length(test_arm)
   } else {
@@ -235,96 +202,6 @@ count_central_steps <- function(test_arm) {
   }
 
 }
-
-
-# Or: vector subtraction of the values at the central indices; or of the entire
-# vectors? Binary search could come in when looking for the first `NA` instead
-# of going from the middle out to the left and right.
-
-
-
-# median_count_na_ignore_old <- function(x) {
-#   n <- length(x)
-#   x <- sort(x[!is.na(x)])
-#   nna <- n - length(x)
-#   half <- if (n %% 2L == 1L) {
-#     (n + 1L) %/% 2L
-#   } else {
-#     (n + 1L:2L) %/% 2L
-#   }
-#   if (n == nna) {
-#     return(nna)
-#   } else if (length(x) > nna && isTRUE(all(x[half - nna] == x[half]))) {
-#     return(0L)
-#   }
-#   steps_through_na <- if (all(x == x[1L])) {
-#     nna - length(x) + 1L
-#   } else {
-#     match(x[1L], rev(x))
-#   }
-#   min(nna, steps_through_na)
-# }
-
-# n_side <- n %/% 2L
-# (nna + 1L) - n_side
-# na_all <- rep(x[NA_integer_], nna)
-# nna_reduced <- nna - length(which(c(na_all, x) == c(x, na_all)))
-# max(0L, nna_reduced)
-# half <- if (n %% 2L == 1L) {
-#   (n + 1L) %/% 2L
-# } else {
-#   (n + 1L:2L) %/% 2L
-# }
-# x[half - nna] == x[half]
-# na_all <- rep(x[NA_integer_], nna)
-# nna_reduced <- nna - length(which(c(na_all, x) == c(x, na_all)))
-# max(0L, nna_reduced)
-# n_side <- n %/% 2L
-# min(nna, match(x[1L], rev(x)))
-# half_upper <- if (n %% 2L == 1L) {
-#   n_side + 2L
-# } else {
-#   n_side + 1L
-# }
-# # Starting from the inside, going out along the non-`NA` paths:
-# side_left <- x[n_side:1L]
-# side_right <- x[nna + (half_upper:length(x))]
-# first_match <- match(side_right[1L], side_left)
-# if (is.na(first_match)) {
-#   return(nna)
-# }
-# max(0L, nna - first_match)
-# if (nna >= n_side) {
-#   return(nna)
-# }
-# min(nna, )
-# half <- x[n_side]
-# half_offset <- x[n_side - nna]
-# frequency_first <- length(x[x == x[1L]])
-# max(0L, nna - frequency_first + 1L)
-
-
-# half <- (n + 1L) %/% 2L
-# offset <- n %% 2L
-# steps_back <- match(x[1L], rev(x))
-
-# first_from_back <- length(x) - match(x[1L], rev(x)) + 1L
-# steps_back <- max(0L, first_from_back + nna)
-# return(steps_back)
-# min(first_from_back, nna)
-# max(0L, steps_back)
-# max(0L, nna - length(x) + steps_back)
-# x[half - nna]
-
-# # I thought this would be needed above, but it doesn't seem to be:
-# if (length(x) %% 2L == 1L) {
-#   steps_back <- 1L + match(x[1L], rev(x))
-#   min(steps_back, nna)
-# } else {
-#   half_lower <- n / 2L
-#   half_lower_all_na_less <- half_lower - nna
-#   # ...
-# }
 
 
 # For interactive use -- shows what the algorithm must be able to get behind:
