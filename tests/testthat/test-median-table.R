b6743c790d805196588f9481706ddf61a9bac30d
Lukas Jung <jung-lukas@gmx.net>
2025-03-23 21:19:58 +0100

    update `median_table()` tests

--- tests/testthat/test-median-table.R
+++ tests/testthat/test-median-table.R
@@ -14,6 +14,8 @@ vec1_exp <- tibble::tibble(
   term = c("a", "b", "c", "d"),
   estimate = c(8, 1, 4, 24),
   certainty = rep(c(TRUE, FALSE), each = 2L),
+  min = c(8, 1, NA, 13.5),
+  max = c(8, 1, NA, 60),
   na_ignored = c(0L, 0L, 3L, 1L),
   na_total = c(0L, 1L, 4L, 1L),
   rate_ignored_na = c(NaN, 0, 0.75, 1),
@@ -24,12 +26,14 @@ vec1_exp <- tibble::tibble(
 vec2_exp <- tibble::tibble(
   term = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"),
   estimate = c(5.8, 3, 4.35, 1.3),
-  certainty = rep(TRUE, 4L),
-  na_ignored = integer(4),
-  na_total = integer(4),
-  rate_ignored_na = rep(NaN, 4L),
-  sum_total = rep(150L, 4L),
-  rate_ignored_sum = numeric(4),
+  certainty = TRUE,
+  min = c(5.8, 3, 4.35, 1.3),
+  max = c(5.8, 3, 4.35, 1.3),
+  na_ignored = 0L,
+  na_total = 0L,
+  rate_ignored_na = NaN,
+  sum_total = 150L,
+  rate_ignored_sum = 0,
 )
 
 
