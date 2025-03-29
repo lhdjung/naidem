4930de9ad068c5d935ef7a5ff1e0b50bfcfb8484
Lukas Jung <jung-lukas@gmx.net>
2025-03-29 19:47:43 +0100

    transform into `median_count_tolerable()`
    * This removes `median_count_na_ignore()` and replaces it by a new function that counts the `NA`s that *can* be preserved when computing the median.

--- R/median-count-na-ignore.R
+++ R/median-count-na-ignore.R
@@ -1,54 +1,46 @@
-#' How many `NA`s need to be ignored for a median estimate?
+#' How many `NA`s can be tolerated for a median estimate?
 #'
-#' @description `median_count_na_ignore()` returns the number of missing values
-#'   that need to be ignored to safely determine the median of the remaining
-#'   values.
+#' @description `median_count_tolerable()` returns the number of missing values
+#'   that can be preserved while determining the median.
 #'
-#'   The point is to retain as many `NA`s as possible when looking for a median
-#'   estimate, instead of simply ignoring all of them.
+#'   The point is to retain as many data points as possible when looking for a
+#'   median estimate, instead of simply ignoring all `NA`s.
 #'
 #' @param x A numeric vector or similar.
-#' @param format String. If `"total"` (the default), the function returns the
-#'   absolute number of `NA`s that must be ignored. If `"proportion"`, it
-#'   returns that number divided by the count of all `NA`s.
-#' @param nna Integer. Ignore unless the function is used as a helper. If
-#'   missing values in `x` were counted elsewhere and then removed, specify
-#'   `nna` as that number to avoid redundantly dealing with `NA`s again. In this
-#'   case, `x` must already be sorted and no longer have any `NA`s! Default is
-#'   to deal with `NA`s within this function.
+#' @param needs_prep Logical. Ignore unless the function is used as a helper.
+#'   See details.
 #'
-#' @details If you specify `nna`, make sure that all `NA`s were already removed
-#'   from `x`, and that `x` is sorted. The function won't check for either, and
-#'   if you don't get this right, it may silently return wrong results.
+#' @details With the default `needs_prep = TRUE`, missing values will be removed
+#'   from `x`, and `x` will be sorted. If this was already done elsewhere,
+#'   setting `needs_prep` to `FALSE` allows you to avoid redundancy and
+#'   inefficiency. Proceed with caution as this is not checked! If you don't get
+#'   this right, the function may silently return wrong results.
 #'
-#'   The main purpose is to speed up [`median_table()`], which uses the present
-#'   function as a helper. Although both are exported, [`median_table()`] is
-#'   generally more useful.
+#'   The main purpose of `needs_prep` is to speed up [`median_table()`], which
+#'   uses the present function as a helper. Although both are exported,
+#'   `median_table()`, is generally more useful.
 #'
-#' @return Numeric (length 1), never `NA`. The value depends on `format`:
-#'   - With the default `format = "total"`, it is an integer that ranges from
-#'   `0` to `length(which(is.na(x)))`, i.e., the total number of missing values
-#'   in `x`.
-#'   - With `format = "proportion"`, it is a double that ranges from `0` to `1`.
+#' @return Integer (length 1). Never `NA`, never negative.
 #'
 #' @export
 #'
 #' @examples
-#' # The median can only be `8`, so
-#' # there is no need to ignore the `NA`:
-#' median_count_na_ignore(c(8, 8, 8, NA))
+#' # With two or fewer `NA`s, the median can only be `8`,
+#' # so these `NA`s are tolerated:
+#' median_count_tolerable(c(8, 8, 8, NA, NA))
 #'
-#' # Here, the median depends on the value
-#' # behind `NA`, so the `NA` really must be ignored:
-#' median_count_na_ignore(c(8, 9, 9, NA))
+#' # When adding a third `NA`, the median will be unknown.
+#' # Compare using naidem's correct median function:
+#' median2(c(8, 8, 8, NA, NA))
+#' median2(c(8, 8, 8, NA, NA, NA))
 #'
-#' # This vector can take two `NA`s, so only one
-#' # of the three needs to be ignored:
-#' median_count_na_ignore(c(8, 8, 8, NA, NA, NA))
-#'
-#' # All need to be ignored here because
+#' # No `NA`s are tolerable here because
 #' # a single one could change the median:
-#' median_count_na_ignore(c(8, 9, NA, NA, NA))
+#' median_count_tolerable(c(8, 9, 9, NA, NA, NA))
+#'
+#' # Here too, the median depends on the value behind `NA`,
+#' # so the `NA` cannot be tolerated:
+#' median_count_tolerable(c(8, 9, NA))
 
 
 # x <- c(1, 7, 7, 7, 7, NA)
@@ -56,38 +48,25 @@
 # x <- c(8, 8, 9, 9, NA, NA, NA)
 
 
-median_count_na_ignore <- function(x,
-                                   format = c("total", "proportion"),
-                                   nna = NULL) {
-  format <- match.arg(format)
-  n <- length(x)
+median_count_tolerable <- function(x, needs_prep = TRUE) {
 
-  # When `median_count_na_ignore()` is used as a helper, the calling function
-  # will likely count `NA`s itself, as well as remove them from `x` and then
-  # sort `x`. Doing so again here would be redundant and inefficient. The `nna`
-  # argument allows the caller to pass this number to the present function
-  # instead. This requires `x` to no longer have any `NA`s, and to be sorted!
-  if (is.null(nna)) {
-    # This removes all `NA`s:
-    x <- sort(x, na.last = NA)
-    n_known <- length(x)
-    nna <- n - n_known
-  } else {
-    nna <- as.integer(nna)
-    n_known <- n
-    n <- n + nna
+  # When `median_count_tolerable()` is used as a helper, the calling function
+  # will likely remove `NA`s from `x` and then sort `x`. Redundantly doing so
+  # again here would be inefficient. The optional `needs_prep` argument allows
+  # the caller to skip this step. Note: this requires `x` to no longer have any
+  # `NA`s, and to be sorted!
+  if (needs_prep) {
+    x <- sort(x[!is.na(x)])
   }
 
-  # Special rules apply with extreme (low or high) numbers of missing values:
-  # -- No missing values, nothing to ignore.
-  # -- All values are missing, all must be ignored. This is recursive: remove
-  # one, and the median is still unknown.
-  if (nna == 0L) {
+  # In case all values are missing, none of them are tolerable. This is
+  # recursive: remove one, and the median is still unknown.
+  if (length(x) == 0L) {
     return(0L)
-  } else if (nna == n) {
-    return(nna)
   }
 
+  # From here on, the function will only work with the known (non-`NA`) values.
+  n_known <- length(x)
   n_known_is_even <- n_known %% 2L == 0L
 
   if (n_known_is_even) {
@@ -96,12 +75,12 @@ median_count_na_ignore <- function(x,
     half_lower <- n_known %/% 2L
     half_upper <- half_lower + 1L
     # In case of two unequal central values, even just a single `NA` can shift
-    # the median, so all `NA`s must be ignored. This can only occur with an even
-    # number of known values. The `near()` function is like `==` except it
+    # the median, so none of them are tolerable. This can only occur with an
+    # even number of known values. The `near()` function is like `==` except it
     # ignores spurious differences that arise due to floating point errors.
     # E.g., `0.1 + 0.2 == 0.3` is `FALSE` but `near(0.1 + 0.2, 0.3)` is `TRUE`.
     if (!near(x[half_lower], x[half_upper])) {
-      return(nna)
+      return(0L)
     }
     # With an odd number of known values, there is only one central index for
     # them. Even so, however, this single value is assigned to two variables
@@ -116,27 +95,25 @@ median_count_na_ignore <- function(x,
   # Special rules apply if there are not enough known values to meaningfully
   # count the steps within their center:
   # -- With two known values, they are known to be equal due to the check above.
-  # They tolerate exactly one `NA`, so all but one need to be ignored.
-  # Subtraction can't return a negative number here because the zero-`NA` case
-  # was already handled before.
-  # -- A single known value can't tolerate any missing values, so all of them
-  # must be ignored. At this point, `n_known` can only be one, not zero, because
-  # of the checks above.
+  # They tolerate exactly one `NA`. Subtraction can't return a negative number
+  # here because the zero-`NA` case was already handled before.
+  # -- A single known value can't tolerate any missing values. At this point,
+  # `n_known` can only be one, not zero, because of the checks above.
   if (n_known < 3L) {
     if (n_known == 2L) {
-      return(nna - 1L)
+      return(1L)
     }
-    return(nna)
+    return(0L)
   }
 
   # Using a helper function, count the steps from the central value outward in
   # each direction where the value is still the same as in the center. Maybe
   # read the `steps_right` part first; it is certainly more intuitive.
 
-  # For the left-hand arm, `seq_len()` creates a vector from 1 to the first
-  # value to the left of the lower central value, and `rev()` then reverses it,
-  # so that `x[half_lower]` will first be compared to the first value to its
-  # left, then the next value on the left, and so on.
+  # -- For the left arm, `seq_len()` creates a vector from 1 to the first value
+  # to the left of the lower central value, and `rev()` then reverses it, so
+  # that `x[half_lower]` will first be compared to the first value to its left,
+  # then the next value on the left, and so on.
   steps_left <- count_central_steps(
     near(
       x[half_lower],
@@ -144,8 +121,8 @@ median_count_na_ignore <- function(x,
     )
   )
 
-  # For the right-hand arm, start at the first value to the right of the upper
-  # central value, and go from there to `n_known`; the total number of known
+  # -- For the right arm, start at the first value to the right of the upper
+  # central value and go from there to `n_known`; the total number of known
   # values. In this way, the upper central value will be compared successively
   # to the values to its right, until a different value is found. See the docs
   # for `count_central_steps()` below this function.
@@ -175,26 +152,11 @@ median_count_na_ignore <- function(x,
   # Note that (1, 2) and (1, 2, 3, 4) do NOT count -- the two central indices
   # are unequal, so they would have been filtered out earlier!
 
-  nna_tolerated <- if (n_known_is_even) {
+  if (n_known_is_even) {
     min(steps_left, steps_right) * 2L + 1L
   } else {
     min(steps_left, steps_right) * 2L
   }
-
-  # Subtract this quantity from the total number of missing values to get the
-  # number of `NA`s that are *not* tolerated, and therefore need to be ignored.
-  # If this number is greater than `nna` -- i.e., if the distribution of known
-  # values could tolerate more `NA`s than there actually are -- the resulting
-  # negative number needs to be replaced by zero because there can't be a
-  # negative amount of values.
-  nna_ignored <- max(0L, nna - nna_tolerated)
-
-  # Return the result, either as an absolute number or as a share of all `NA`s:
-  switch(
-    format,
-    "total"      = nna_ignored,
-    "proportion" = nna_ignored / nna
-  )
 
 }
 
@@ -202,7 +164,7 @@ median_count_na_ignore <- function(x,
 #' Count steps from the central value where the same value is still found
 #'
 #' @description The internal helper `count_central_steps()` is used within
-#'   `median_count_na_ignore()`.
+#'   `median_count_tolerable()`.
 #'
 #'   The function starts from one of the two central values (upper or lower) of
 #'   the sorted distribution after all `NA`s were removed. It counts the steps
