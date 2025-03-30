f38317084b5126a61b0c96dc792f82cbee6e1b5d
Lukas Jung <jung-lukas@gmx.net>
2025-03-30 03:02:56 +0200

    new ideas for `median_print()`

--- R/median-print.R
+++ R/median-print.R
@@ -4,6 +4,13 @@ median_print <- function(x) {
   x <- sort(x[!is.na(x)])
   nna <- n - length(x)
   na_all <- rep(x[NA_integer_], nna)
+
+  xstr <- as.character(x)
+  nchar_max <- max(
+    vapply(xstr, nchar, integer(1), USE.NAMES = FALSE),
+    2L  # number of characters in "NA"
+  )
+
   if (n %% 2L == 1L) {
     half <- (n + 1L) %/% 2L
     # n_arm <- half - 1L
@@ -12,12 +19,12 @@ median_print <- function(x) {
     # n_arm <- half[1L] - 1L
   }
   n_arm <- half[1L] - 1L
-  central_highlight <- paste0(rep(" ", 4), collapse = "")
+  central_highlight <- paste0(rep(" ", nchar_max), collapse = "")
   line_outer <- paste0(rep("--", times = n_arm), collapse = "")
   line_whole <- paste0(line_outer, central_highlight, line_outer, collapse = "")
   # Now, the printing:
-  print(line_whole, quote = FALSE)
-  print(c(na_all, x))
-  print(c(x, na_all))
-  print(line_whole, quote = FALSE)
+  cat(c(line_whole, "\n"))
+  print(c(na_all, x, "\n"))
+  print(c(x, na_all, "\n"))
+  cat(line_whole)
 }
