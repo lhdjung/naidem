ad5ba7b93b608421607361114cc374d07addff53
Lukas Jung <jung-lukas@gmx.net>
2025-05-03 20:06:01 +0200

    add missing tests for `median_count_tolerable()`

--- tests/testthat/test-median-count-tolerable.R
+++ tests/testthat/test-median-count-tolerable.R
@@ -10,10 +10,17 @@ test_that("`median_count_tolerable()` returns correct values for odd length", {
 test_that("`median_count_tolerable()` returns correct values for even length", {
   expect_equal(median_count_tolerable(c(1, 1)), 1L)
   expect_equal(median_count_tolerable(c(1, NA)), 0L)
+  expect_equal(median_count_tolerable(c(1, 1, 1, 2)), 1L)
+  expect_equal(median_count_tolerable(c(1, 1, 1, 1)), 3L)
+  expect_equal(median_count_tolerable(c(1, 2, 2, 3)), 1L)
+  expect_equal(median_count_tolerable(c(1, 1, 1, 1, 2, 2)), 1L)
+  expect_equal(median_count_tolerable(c(1, 1, 2, 2, 3, 3)), 1L)
+  expect_equal(median_count_tolerable(c(1, 7, 7, 7, 7, NA)), 2L)
   expect_equal(median_count_tolerable(c(1, 1, NA, NA)), 1L)
   expect_equal(median_count_tolerable(c(1, 2, NA, NA)), 0L)
   expect_equal(median_count_tolerable(c(1, 1, 2, NA)), 0L)
   expect_equal(median_count_tolerable(c(1, 2, 2, NA)), 0L)
+  expect_equal(median_count_tolerable(c(8, 8, 9, 9)), 0L)
 })
 
 test_that("`median_count_tolerable()` handles non-numeric data correctly", {
